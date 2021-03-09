dd99295d7053e9b1a2ce15d476946a4b397784a6
Tingwei <lydia9017@gmail.com>
2021-03-09 15:33:41 +0800

    object

--- src/main/java/fju/edu/HelloWord.java
+++ src/main/java/fju/edu/HelloWord.java
@@ -2,6 +2,7 @@ package fju.edu;
 
 public class HelloWord {
     public static void main(String[] args) {
+        System.out.println("Hello world");
 
     }
 }
---
+++ src/main/java/fju/edu/Student.java
@@ -0,0 +1,4 @@
+package fju.edu;
+
+public class Student {
+}
